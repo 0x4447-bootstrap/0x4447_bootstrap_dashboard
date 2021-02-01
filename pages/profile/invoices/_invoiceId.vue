0108b16aebc8eea26e43e5abf98436e87563c681
Vitalii Cherniuk <acidbiscuit@gmail.com>
2021-02-02 01:08:44 +0800

    bug: #139 invoice show full invoice and charge ids

--- pages/profile/invoices/_invoiceId.vue
+++ pages/profile/invoices/_invoiceId.vue
@@ -138,13 +138,13 @@ export default {
       return [
         {
           label: 'Invoice ID',
-          value: invoice.stripe_invoice_id?.substring(3) || 'N/A',
-          copy: true
+          value: invoice.stripe_invoice_id || 'N/A',
+          copy: !!invoice.stripe_invoice_id
         },
         {
           label: 'Charge ID',
-          value: invoice.charge_id?.substring(3) || 'N/A',
-          copy: true
+          value: invoice.charge_id || 'N/A',
+          copy: !!invoice.stripe_invoice_id
         },
         {
           label: 'Created',
