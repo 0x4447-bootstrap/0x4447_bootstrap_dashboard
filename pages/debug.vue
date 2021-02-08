78475c416e04110e05da5caa645f46dd7e679d61
Vitalii Cherniuk <acidbiscuit@gmail.com>
2021-02-08 15:28:07 +0800

    feat: #162 debug display versioning response

--- pages/debug.vue
+++ pages/debug.vue
@@ -32,11 +32,46 @@
         </v-row>
       </v-card-text>
     </v-card>
+
+    <v-row>
+      <v-col>
+        <h3 class="text-h6 my-3 px-4">
+          Versioning Test
+        </h3>
+
+        <v-card>
+          <v-card-text>
+            <v-row>
+              <a-column>
+                <v-simple-table
+                  dense
+                >
+                  <template #default>
+                    <tbody>
+                      <tr
+                        v-for="(value, key) in versioning"
+                        :key="key"
+                      >
+                        <td>{{ key }}</td>
+                        <td>
+                          {{ value }}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </a-column>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-layout>
 </template>
 
 <script>
 import { Auth } from 'aws-amplify'
+import { mapActions } from 'vuex'
 import LambdaClient from '~/services/aws/Lambda'
 
 export default {
@@ -44,7 +79,8 @@ export default {
 
   data () {
     return {
-      currentCredentials: {}
+      currentCredentials: {},
+      versioning: {}
     }
   },
 
@@ -62,16 +98,30 @@ export default {
   async beforeMount () {
     this.currentCredentials = await Auth.currentCredentials()
 
-    this.notifyDebugVersion()
+    this.versioningTest()
   },
 
   methods: {
-    async notifyDebugVersion () {
+    ...mapActions({
+      notificationShow: 'notifications/show'
+    }),
+
+    async versioningTest () {
       const version = this.$nuxt.context.isDev ? '$LATEST' : this.$config.VERSION_DASHBOARD_DEBUG_VERSION
-      await LambdaClient.invoke({
-        functionName: 'dashboard_debug_version',
-        version
-      })
+
+      try {
+        const { Payload } = await LambdaClient.invoke({
+          functionName: 'dashboard_debug_version',
+          version
+        })
+
+        this.versioning = JSON.parse(Payload)
+      } catch (err) {
+        this.notificationShow({
+          type: 'error',
+          message: 'Unable to retrieve debug version'
+        })
+      }
     }
   },
 
