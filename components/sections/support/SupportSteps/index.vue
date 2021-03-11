a9cadaae13c5c5c9b3ff14c1f64f6832c0065915
abiscuit <account@abiscuit.io>
2021-03-12 05:04:33 +0800

    feat: #221 support steps increase transition duration

--- components/sections/support/SupportSteps/index.vue
+++ components/sections/support/SupportSteps/index.vue
@@ -160,3 +160,17 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.tab-transition-enter,
+.tab-transition-enter-active,
+.tab-transition-leave,
+.tab-transition-leave-active,
+.tab-transition-leave-to,
+.tab-reverse-transition-enter,
+.tab-reverse-transition-enter-active,
+.tab-reverse-transition-leave,
+.tab-reverse-transition-leave-to {
+  transition: 1s cubic-bezier(0.25, 0.8, 0.5, 1) !important;
+}
+</style>
